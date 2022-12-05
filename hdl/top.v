33510a2b315908fb235b173c40e0902e06976b9f
TristenLiu <98703889+TristenLiu@users.noreply.github.com>
2022-12-05 16:22:48 -0500

    Updated top_square to display 00:00:00

--- hdl/top.v
+++ hdl/top.v
@@ -16,64 +16,82 @@ module top(
     output wire VGA_HS,
     output wire VGA_VS
     );
+    wire [35:0] random_num;
+    reg [9:0] hit = 0;
     reg hrEdit, hrLeft, hrRight, hrInc, hrReset;
     reg tEdit, tLeft, tRight, tBTNC, tReset;        //tBTNC = start or increment, depending on toggle mode
     reg swStart, swReset;
+    wire clk_hz;
     // 0 = hour clock, 1 = timer, 2 = stopwatch
     reg [2:0] mode;
     
     top_square ts(.CLK(clk_i), 
                   .RST_BTN(reset_n), 
-                  .random_num(), 
-                  .hit(),
+                  .random_num(random_num), 
+                  .hit(hit),
                   .VGA_HS_O(VGA_HS), 
                   .VGA_VS_O(VGA_VS), 
                   .VGA_R(VGA_R), 
                   .VGA_G(VGA_G), 
                   .VGA_B(VGA_B));
     
-    // start at hour clock mode
-    initial begin
-        mode = 2'b00;
-        hrEdit = BTND;
-        hrLeft = BTNL;
-        hrRight = BTNR;
-        hrInc = BTNC;
-        hrReset = reset_n;
-    end
+    clock_divider cd0(.clk(clk_i), .div_clk(clk_hz));
+    counter24 cnt(.clk_i(clk_hz), .resetn_i(reset_n), .count_o(random_num));
     
-    // BTNU is used to cycle between the modes, and choose which wires the input buttons will map to
-    // Make sure any button presses in mode x does not effect the state of mode y or mode z
-    always @(BTNU) begin
-        case (mode)
-            2'b00: begin
-                mode = 2'b01;
-                hrEdit = 0; hrLeft = 0; hrRight = 0; hrInc = 0; hrReset = 1;
-                tBTNC = BTNC;
-                tEdit = BTND;
-                tLeft = BTNL;
-                tRight = BTNR;
-                tReset = reset_n;
-            end
-            2'b01: begin
-                mode = 2'b10;
-                tBTNC = 0; tEdit = 0; tLeft = 0; tRight = 0; tReset = 1;
-                swStart = BTNC;
-                swReset = reset_n;
-            end
-            2'b10: begin
-                mode = 2'b00;
-                swStart = 0; swReset = 1;
-                hrEdit = BTND;
-                hrLeft = BTNL;
-                hrRight = BTNR;
-                hrInc = BTNC;
-                hrReset = reset_n;
-            end
-            default: //if mode is somehow not in those three options, reset to hour clock
-                mode = 2'b00;
-        endcase
-    end //always
+//    debouncer db0(.clk_i(clk_i), .resetn_btn_i(reset_n), .increment_counter_btn_i(BTNL), .btn_o(debnum));
+//    debouncer db1(.clk_i(clk_i), .resetn_btn_i(reset_n), .increment_counter_btn_i(BTNR), .btn_o(debhit));
+    
+//    always @(posedge clk_i) begin
+//        if (debnum) begin
+//            if (random_num == 0)
+//                random_num = 1'b1;
+//            else random_num = random_num << 1;
+//        end
+//        //if (debhit) hit = hit + 1'b1;
+//    end //always
+    
+//    // start at hour clock mode
+//    initial begin
+//        mode = 2'b00;
+//        hrEdit = BTND;
+//        hrLeft = BTNL;
+//        hrRight = BTNR;
+//        hrInc = BTNC;
+//        hrReset = reset_n;
+//    end //initial
+    
+//    // BTNU is used to cycle between the modes, and choose which wires the input buttons will map to
+//    // Make sure any button presses in mode x does not effect the state of mode y or mode z
+//    always @(BTNU) begin
+//        case (mode)
+//            2'b00: begin
+//                mode = 2'b01;
+//                hrEdit = 0; hrLeft = 0; hrRight = 0; hrInc = 0; hrReset = 1;
+//                tBTNC = BTNC;
+//                tEdit = BTND;
+//                tLeft = BTNL;
+//                tRight = BTNR;
+//                tReset = reset_n;
+//            end
+//            2'b01: begin
+//                mode = 2'b10;
+//                tBTNC = 0; tEdit = 0; tLeft = 0; tRight = 0; tReset = 1;
+//                swStart = BTNC;
+//                swReset = reset_n;
+//            end
+//            2'b10: begin
+//                mode = 2'b00;
+//                swStart = 0; swReset = 1;
+//                hrEdit = BTND;
+//                hrLeft = BTNL;
+//                hrRight = BTNR;
+//                hrInc = BTNC;
+//                hrReset = reset_n;
+//            end
+//            default: //if mode is somehow not in those three options, reset to hour clock
+//                mode = 2'b00;
+//        endcase
+//    end //always
     
     
 endmodule
--- hdl/top_square.v
+++ hdl/top_square.v
@@ -21,8 +21,8 @@
 module top_square(
     input wire CLK,             // board clock: 100 MHz on Arty/Basys3/Nexys
     input wire RST_BTN,
-    input wire [7:0] random_num,
-    input wire [7:0] hit,         
+    input wire [35:0] random_num,
+    input wire [9:0] hit,         
     output wire VGA_HS_O,       // horizontal sync output
     output wire VGA_VS_O,       // vertical sync output
     output wire [3:0] VGA_R,    // 4-bit VGA red output
@@ -53,43 +53,102 @@ module top_square(
     );
 
     // Wires to hold regions on FPGA
-    wire SQ1,SQ2,SQ3,SQ4,SQ5,SQ6,SQ7,SQ8,SQ9,SQ10,SQ11,SQ12,SQ13,SQ14,SQ15,SQ16,SQ17,SQMid,SQ10hit,SQ11hit,SQ12hit,SQ13hit,SQ14hit,SQ15hit,SQ16hit,SQ17hit;
+    wire fullscreen;
+    wire [8:0] leftdigit_blank;
+    wire [7:0] leftdigit_ss;
+    wire [12:0] h2, h1, m2, m1, s2, s1, ms3, ms2, ms1;
+    wire [1:0] c2, c1;
 	
     //Registers for entities
 	reg green,grid;
+	reg [10:0] sum;
 	
 	// Creating Regions on the VGA Display represented as wires (640x480)
 	
 	// SQ1 is a large Square, and SQ2-9, along with SQ Mid are areas within SQ1
-    assign SQ1 = ((x > 100) & (y > 60) & (x < 540) & (y < 420)) ? 1 : 0;
-    assign SQ2 = ((x > 180) & (y > 60) & (x < 280) & (y < 420)) ? 1 : 0; 
-    assign SQ3 = ((x > 360) & (y > 60) & (x < 460) & (y < 420)) ? 1 : 0; 
-    assign SQ4 = ((x > 100) & (y > 279) & (x < 181) & (y < 341)) ? 1 : 0; // Red Square
-    assign SQ5 = ((x > 100) & (y > 139) & (x < 181) & (y < 201)) ? 1 : 0; // Red Square
-    assign SQ6 = ((x > 279) & (y > 279) & (x < 361) & (y < 341)) ? 1 : 0; // Red Square
-    assign SQ7 = ((x > 279) & (y > 139) & (x < 361) & (y < 201)) ? 1 : 0; // Red Square
-    assign SQ8 = ((x > 459) & (y > 279) & (x < 540) & (y < 341)) ? 1 : 0; // Red Square
-    assign SQ9 = ((x > 459) & (y > 139) & (x < 540) & (y < 201)) ? 1 : 0; // Red Square
-    assign SQMid = ((x > 279) & (y > 200) & (x < 361) & (y < 280)) ? 1 : 0; // Center Square
+    assign fullscreen = ((x > 52 ) & (x < 573) & (y > 179) & (y < 264)) ? 1 : 0;
+    assign h2[0] = ((x > 52) & (x < 111) & (y > 179) & (y < 194)) ? 1 : 0;
+    assign h2[1] = ((x > 95) & (x < 111) & (y > 179) & (y < 229)) ? 1 : 0;
+    assign h2[2] = ((x > 95) & (x < 111) & (y > 214) & (y < 262)) ? 1 : 0;
+    assign h2[3] = ((x > 52) & (x < 111) & (y > 249) & (y < 264)) ? 1 : 0;
+    assign h2[4] = ((x > 52) & (x < 69) & (y > 214) & (y < 262)) ? 1 : 0;
+    assign h2[5] = ((x > 52) & (x < 69) & (y > 179) & (y < 229)) ? 1 : 0;
+    assign h2[6] = ((x > 52) & (x < 111) & (y > 214) & (y < 229)) ? 1 : 0;
     
-	// SQ10-17 are also areas within SQ1
-    assign SQ10 = ((x > 100) & (y > 60) &  (x < 181) & (y < 140)) ? 1 : 0; // Green Square
-    assign SQ11 = ((x > 100) & (y > 200) & (x < 181) & (y < 280)) ? 1 : 0; // Green Square
-    assign SQ12 = ((x > 100) & (y > 340) & (x < 181) & (y < 420)) ? 1 : 0; // Green Square
-    assign SQ13 = ((x > 279) & (y > 60 ) & (x < 361) & (y < 140)) ? 1 : 0; // Green Square
-    assign SQ14 = ((x > 279) & (y > 340) & (x < 361) & (y < 420)) ? 1 : 0; // Green Square
-    assign SQ15 = ((x > 459) & (y > 60 ) & (x < 540) & (y < 140)) ? 1 : 0; // Green Square
-    assign SQ16 = ((x > 459) & (y > 200) & (x < 540) & (y < 280)) ? 1 : 0; // Green Square
-    assign SQ17 = ((x > 459) & (y > 340) & (x < 540) & (y < 420)) ? 1 : 0; // Green Square
-    // SQ10hit-17hit are the same areas as SQ10-17
-    assign SQ10hit = ((x > 100) & (y > 60) &  (x < 181) & (y < 140)) ? 1 : 0; // Hit Square
-    assign SQ11hit = ((x > 100) & (y > 200) & (x < 181) & (y < 280)) ? 1 : 0; // Hit Square
-    assign SQ12hit = ((x > 100) & (y > 340) & (x < 181) & (y < 420)) ? 1 : 0; // Hit Square
-    assign SQ13hit = ((x > 279) & (y > 60 ) & (x < 361) & (y < 140)) ? 1 : 0; // Hit Square
-    assign SQ14hit = ((x > 279) & (y > 340) & (x < 361) & (y < 420)) ? 1 : 0; // Hit Square
-    assign SQ15hit = ((x > 459) & (y > 60 ) & (x < 540) & (y < 140)) ? 1 : 0; // Hit Square
-    assign SQ16hit = ((x > 459) & (y > 200) & (x < 540) & (y < 280)) ? 1 : 0; // Hit Square
-    assign SQ17hit = ((x > 459) & (y > 340) & (x < 540) & (y < 420)) ? 1 : 0; // Hit Square
+    assign h1[0] = ((x > 69 + 52) & (x < 69 + 111) & (y > 179) & (y < 194)) ? 1 : 0;
+    assign h1[1] = ((x > 69 + 95) & (x < 69 + 111) & (y > 179) & (y < 229)) ? 1 : 0;
+    assign h1[2] = ((x > 69 + 95) & (x < 69 + 111) & (y > 214) & (y < 262)) ? 1 : 0;
+    assign h1[3] = ((x > 69 + 52) & (x < 69 + 111) & (y > 249) & (y < 264)) ? 1 : 0;
+    assign h1[4] = ((x > 69 + 52) & (x < 69 + 69) & (y > 214) & (y < 262)) ? 1 : 0;
+    assign h1[5] = ((x > 69 + 52) & (x < 69 + 69) & (y > 179) & (y < 229)) ? 1 : 0;
+    assign h1[6] = ((x > 69 + 52) & (x < 69 + 111) & (y > 214) & (y < 229)) ? 1 : 0;
+    
+    assign m2[0] = ((x > 166 + 52) & (x < 166 + 111) & (y > 179) & (y < 194)) ? 1 : 0;
+    assign m2[1] = ((x > 166 + 95) & (x < 166 + 111) & (y > 179) & (y < 229)) ? 1 : 0;
+    assign m2[2] = ((x > 166 + 95) & (x < 166 + 111) & (y > 214) & (y < 262)) ? 1 : 0;
+    assign m2[3] = ((x > 166 + 52) & (x < 166 + 111) & (y > 249) & (y < 264)) ? 1 : 0;
+    assign m2[4] = ((x > 166 + 52) & (x < 166 + 69) & (y > 214) & (y < 262)) ? 1 : 0;
+    assign m2[5] = ((x > 166 + 52) & (x < 166 + 69) & (y > 179) & (y < 229)) ? 1 : 0;
+    assign m2[6] = ((x > 166 + 52) & (x < 166 + 111) & (y > 214) & (y < 229)) ? 1 : 0;
+    
+    assign m1[0] = ((x > 235 + 52) & (x < 235 + 111) & (y > 179) & (y < 194)) ? 1 : 0;
+    assign m1[1] = ((x > 235 + 95) & (x < 235 + 111) & (y > 179) & (y < 229)) ? 1 : 0;
+    assign m1[2] = ((x > 235 + 95) & (x < 235 + 111) & (y > 214) & (y < 262)) ? 1 : 0;
+    assign m1[3] = ((x > 235 + 52) & (x < 235 + 111) & (y > 249) & (y < 264)) ? 1 : 0;
+    assign m1[4] = ((x > 235 + 52) & (x < 235 + 69) & (y > 214) & (y < 262)) ? 1 : 0;
+    assign m1[5] = ((x > 235 + 52) & (x < 235 + 69) & (y > 179) & (y < 229)) ? 1 : 0;
+    assign m1[6] = ((x > 235 + 52) & (x < 235 + 111) & (y > 214) & (y < 229)) ? 1 : 0;
+    
+    assign s2[0] = ((x > 332 + 52) & (x < 332 + 111) & (y > 179) & (y < 194)) ? 1 : 0;
+    assign s2[1] = ((x > 332 + 95) & (x < 332 + 111) & (y > 179) & (y < 229)) ? 1 : 0;
+    assign s2[2] = ((x > 332 + 95) & (x < 332 + 111) & (y > 214) & (y < 262)) ? 1 : 0;
+    assign s2[3] = ((x > 332 + 52) & (x < 332 + 111) & (y > 249) & (y < 264)) ? 1 : 0;
+    assign s2[4] = ((x > 332 + 52) & (x < 332 + 69) & (y > 214) & (y < 262)) ? 1 : 0;
+    assign s2[5] = ((x > 332 + 52) & (x < 332 + 69) & (y > 179) & (y < 229)) ? 1 : 0;
+    assign s2[6] = ((x > 332 + 52) & (x < 332 + 111) & (y > 214) & (y < 229)) ? 1 : 0;
+    
+    assign s1[0] = ((x > 401 + 52) & (x < 401 + 111) & (y > 179) & (y < 194)) ? 1 : 0;
+    assign s1[1] = ((x > 401 + 95) & (x < 401 + 111) & (y > 179) & (y < 229)) ? 1 : 0;
+    assign s1[2] = ((x > 401 + 95) & (x < 401 + 111) & (y > 214) & (y < 262)) ? 1 : 0;
+    assign s1[3] = ((x > 401 + 52) & (x < 401 + 111) & (y > 249) & (y < 264)) ? 1 : 0;
+    assign s1[4] = ((x > 401 + 52) & (x < 401 + 69) & (y > 214) & (y < 262)) ? 1 : 0;
+    assign s1[5] = ((x > 401 + 52) & (x < 401 + 69) & (y > 179) & (y < 229)) ? 1 : 0;
+    assign s1[6] = ((x > 401 + 52) & (x < 401 + 111) & (y > 214) & (y < 229)) ? 1 : 0;
+    
+    assign c2[0] = ((x > 190) & (x < 207) & (y > 209) & (y < 225)) ? 1 : 0;
+    assign c2[1] = ((x > 190) & (x < 207) & (y > 247) & (y < 264)) ? 1 : 0;
+    
+    assign c1[0] = ((x > 166 + 190) & (x < 166 + 207) & (y > 209) & (y < 225)) ? 1 : 0;
+    assign c1[1] = ((x > 166 + 190) & (x < 166 + 207) & (y > 247) & (y < 264)) ? 1 : 0;
+    
+//    assign leftdigit_blank[0] = ((x > 40 ) & (y > 100) & (x < 60 ) & (y < 120)) ? 1 : 0; 
+//    assign leftdigit_blank[1] = ((x > 40 ) & (y > 180) & (x < 60 ) & (y < 200)) ? 1 : 0; 
+//    assign leftdigit_blank[2] = ((x > 40 ) & (y > 260) & (x < 60 ) & (y < 280)) ? 1 : 0; // Red Square
+//    assign leftdigit_blank[3] = ((x > 60 ) & (y > 120) & (x < 120) & (y < 180)) ? 1 : 0; // Red Square
+//    assign leftdigit_blank[4] = ((x > 60 ) & (y > 200) & (x < 120) & (y < 260)) ? 1 : 0; // Red Square
+//    assign leftdigit_blank[5] = ((x > 120) & (y > 100) & (x < 140) & (y < 120)) ? 1 : 0; // Red Square
+//    assign leftdigit_blank[6] = ((x > 120) & (y > 180) & (x < 140) & (y < 200)) ? 1 : 0; // Red Square
+//    assign leftdigit_blank[7] = ((x > 120) & (y > 260) & (x < 140) & (y < 280)) ? 1 : 0; // Red Square
+//    assign SQMid = ((x > 140) & (y > 100) & (x < 600) & (y < 280)) ? 1 : 0; // Center Square
+    
+//	// SQ10-17 are also areas within SQ1
+//    assign leftdigit_ss[0] = ((x > 60 ) & (y > 100) & (x < 120) & (y < 120)) ? 1 : 0; // Green Square              
+//    assign leftdigit_ss[1] = ((x > 120) & (y > 120) & (x < 140) & (y < 180)) ? 1 : 0; // Green Square
+//    assign leftdigit_ss[2] = ((x > 120) & (y > 200) & (x < 140) & (y < 260)) ? 1 : 0; // Green Square
+//    assign leftdigit_ss[3] = ((x > 60 ) & (y > 260) & (x < 120) & (y < 280)) ? 1 : 0; // Green Square
+//    assign leftdigit_ss[4] = ((x > 40 ) & (y > 200) & (x < 60 ) & (y < 260)) ? 1 : 0; // Green Square
+//    assign leftdigit_ss[5] = ((x > 40 ) & (y > 120) & (x < 60 ) & (y < 180)) ? 1 : 0; // Green Square
+//    assign leftdigit_ss[6] = ((x > 60 ) & (y > 180) & (x < 120) & (y < 200)) ? 1 : 0; // Green Square
+//    assign SQ17 = ((x > 459) & (y > 340) & (x < 540) & (y < 420)) ? 1 : 0; // Green Square
+//    // SQ10hit-17hit are the same areas as SQ10-17
+// assign SQ10hit = ((x > 60 ) & (y > 100) & (x < 120) & (y < 120)) ? 1 : 0; // Hit Square
+// assign SQ11hit = ((x > 120) & (y > 120) & (x < 140) & (y < 180)) ? 1 : 0; // Hit Square
+// assign SQ12hit = ((x > 120) & (y > 200) & (x < 140) & (y < 260)) ? 1 : 0; // Hit Square
+// assign SQ13hit = ((x > 60 ) & (y > 260) & (x < 120) & (y < 280)) ? 1 : 0; // Hit Square
+// assign SQ14hit = ((x > 40 ) & (y > 200) & (x < 60 ) & (y < 260)) ? 1 : 0; // Hit Square
+// assign SQ15hit = ((x > 40 ) & (y > 120) & (x < 60 ) & (y < 180)) ? 1 : 0; // Hit Square
+// assign SQ16hit = ((x > 60 ) & (y > 180) & (x < 120) & (y < 200)) ? 1 : 0; // Hit Square
+// assign SQ17hit = ((x > 459) & (y > 340) & (x < 540) & (y < 420)) ? 1 : 0; // Hit Square
 
 
 
@@ -105,85 +164,74 @@ module top_square(
 	
 	//Green = 0 means that there will be no values of x/y on the VGA that will display green
     green = 0;
+    sum = 0;
 	//This statement makes it so that within SQ1, a 3x3 grid of squares appears, with the middle square blacked out
-    grid =  SQ1 - SQ2 - SQ3 - SQ4 - SQ5 - SQ6 - SQ7 - SQ8 - SQ9 - SQMid;
+    grid = 0;
+    //fullscreen - leftdigit_blank[0] - leftdigit_blank[1] - leftdigit_blank[2]
+    //             - leftdigit_blank[3] - leftdigit_blank[4] - leftdigit_blank[5] - leftdigit_blank[6] - leftdigit_blank[7] - SQMid;
+    case(random_num[3:0])
+        4'b0000: sum = sum + s1[0] + s1[1] + s1[2] + s1[3] + s1[4] + s1[5];
+        4'b0001: sum = sum + s1[1] + s1[2];
+        4'b0010: sum = sum + s1[0] + s1[1] + s1[3] + s1[4] + s1[6];
+        4'b0011: sum = sum + s1[0] + s1[1] + s1[2] + s1[3] + s1[6];
+        4'b0100: sum = sum + s1[1] + s1[2] + s1[5] + s1[6];
+        4'b0101: sum = sum + s1[0] + s1[2] + s1[3] + s1[5] + s1[6];
+        4'b0110: sum = sum + s1[0] + s1[2] + s1[3] + s1[4] + s1[5] + s1[6];
+        4'b0111: sum = sum + s1[0] + s1[1] + s1[2];
+        4'b1000: sum = sum + s1[0] + s1[1] + s1[2] + s1[3] + s1[4] + s1[5] + s1[6];
+        4'b1001: sum = sum + s1[0] + s1[1] + s1[2] + s1[5] + s1[6];
+    endcase
+    case(random_num[7:4])
+        4'b0000: sum = sum + s2[0] + s2[1] + s2[2] + s2[3] + s2[4] + s2[5];
+        4'b0001: sum = sum + s2[1] + s2[2];
+        4'b0010: sum = sum + s2[0] + s2[1] + s2[3] + s2[4] + s2[6];
+        4'b0011: sum = sum + s2[0] + s2[1] + s2[2] + s2[3] + s2[6];
+        4'b0100: sum = sum + s2[1] + s2[2] + s2[5] + s2[6];
+        4'b0101: sum = sum + s2[0] + s2[2] + s2[3] + s2[5] + s2[6];
+        4'b0110: sum = sum + s2[0] + s2[2] + s2[3] + s2[4] + s2[5] + s2[6];
+    endcase
+    case(random_num[11:8])
+        4'b0000: sum = sum + m1[0] + m1[1] + m1[2] + m1[3] + m1[4] + m1[5];
+        4'b0001: sum = sum + m1[1] + m1[2];
+        4'b0010: sum = sum + m1[0] + m1[1] + m1[3] + m1[4] + m1[6];
+        4'b0011: sum = sum + m1[0] + m1[1] + m1[2] + m1[3] + m1[6];
+        4'b0100: sum = sum + m1[1] + m1[2] + m1[5] + m1[6];
+        4'b0101: sum = sum + m1[0] + m1[2] + m1[3] + m1[5] + m1[6];
+        4'b0110: sum = sum + m1[0] + m1[2] + m1[3] + m1[4] + m1[5] + m1[6];
+        4'b0111: sum = sum + m1[0] + m1[1] + m1[2];
+        4'b1000: sum = sum + m1[0] + m1[1] + m1[2] + m1[3] + m1[4] + m1[5] + m1[6];
+        4'b1001: sum = sum + m1[0] + m1[1] + m1[2] + m1[5] + m1[6];
+    endcase
+    case(random_num[15:12])
+        4'b0000: sum = sum + m2[0] + m2[1] + m2[2] + m2[3] + m2[4] + m2[5];
+        4'b0001: sum = sum + m2[1] + m2[2];
+        4'b0010: sum = sum + m2[0] + m2[1] + m2[3] + m2[4] + m2[6];
+        4'b0011: sum = sum + m2[0] + m2[1] + m2[2] + m2[3] + m2[6];
+        4'b0100: sum = sum + m2[1] + m2[2] + m2[5] + m2[6];
+        4'b0101: sum = sum + m2[0] + m2[2] + m2[3] + m2[5] + m2[6];
+        4'b0110: sum = sum + m2[0] + m2[2] + m2[3] + m2[4] + m2[5] + m2[6];
+    endcase
+    case(random_num[19:16])
+        4'b0000: sum = sum + h1[0] + h1[1] + h1[2] + h1[3] + h1[4] + h1[5];
+        4'b0001: sum = sum + h1[1] + h1[2];
+        4'b0010: sum = sum + h1[0] + h1[1] + h1[3] + h1[4] + h1[6];
+        4'b0011: sum = sum + h1[0] + h1[1] + h1[2] + h1[3] + h1[6];
+        4'b0100: sum = sum + h1[1] + h1[2] + h1[5] + h1[6];
+        4'b0101: sum = sum + h1[0] + h1[2] + h1[3] + h1[5] + h1[6];
+        4'b0110: sum = sum + h1[0] + h1[2] + h1[3] + h1[4] + h1[5] + h1[6];
+        4'b0111: sum = sum + h1[0] + h1[1] + h1[2];
+        4'b1000: sum = sum + h1[0] + h1[1] + h1[2] + h1[3] + h1[4] + h1[5] + h1[6];
+        4'b1001: sum = sum + h1[0] + h1[1] + h1[2] + h1[5] + h1[6];
+    endcase
+    case(random_num[23:20])
+        4'b0000: sum = sum + h2[0] + h2[1] + h2[2] + h2[3] + h2[4] + h2[5];
+        4'b0001: sum = sum + h2[1] + h2[2];
+    endcase
     
-    if(random_num[0] == 1)
-        begin
-			//Add SQ10 to the areas which will display strong green.
-			//Note: This displays yellow on the display, as red+green = yellow.
-            green = green + SQ10; 
-        end
-    else if(random_num[0] == 0 && hit[0]==1)
-        begin
-			//Add SQ10 to the areas which will display strong green, and remove it from the area that displays 
-			//strong red.
-            green = green + SQ10;
-            grid = grid - SQ10hit;
-        end   
-    if(random_num[1] == 1)
-        begin
-            green = green + SQ11;
-        end
-    else if(random_num[1] == 0 && hit[1]==1)
-        begin
-            green = green + SQ11;
-            grid = grid - SQ11hit;
-        end        
-    if(random_num[2] == 1)
-        begin
-            green = green + SQ12;
-        end
-    else if(random_num[2] == 0 && hit[2]==1)
-        begin
-            green = green + SQ12;
-            grid = grid - SQ12hit;
-        end 
-    if(random_num[3] == 1)
-        begin
-            green = green + SQ13;
-        end
-    else if(random_num[3] == 0 && hit[3]==1)
-        begin
-            green = green + SQ13;
-            grid = grid - SQ13hit;
-        end 
-    if(random_num[4] == 1)
-        begin
-            green = green + SQ14;
-        end
-    else if(random_num[4] == 0 && hit[4]==1)
-        begin
-            green = green + SQ14;
-            grid = grid - SQ14hit;
-        end 
-    if(random_num[5] == 1)
-        begin
-            green = green + SQ15;
-        end
-    else if(random_num[5] == 0 && hit[5]==1)
-        begin
-            green = green + SQ15;
-            grid = grid - SQ15hit;
-        end 
-    if(random_num[6] == 1)
-        begin
-            green = green + SQ16;
-        end
-    else if(random_num[6] == 0 && hit[6]==1)
-        begin
-            green = green + SQ16;
-            grid = grid - SQ16hit;
-        end 
-    if(random_num[7] == 1)
-        begin
-            green = green + SQ17;
-        end
-    else if(random_num[7] == 0 && hit[7]==1)
-        begin
-            green = green + SQ17;
-            grid = grid - SQ17hit;
-        end 
+    // always display the colons
+    sum = sum + c2[1] + c2[0] + c1[1] + c1[0];
+    
+    if (sum > 0) green = 1;
   end
     
     
