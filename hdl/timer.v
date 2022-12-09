2b377e1d25ce4496d6bea468b7acca286b51b0d9
dcthomp <dcthomp@bu.edu>
2022-12-09 17:01:01 -0500

    fixed timer (maybe?)

--- hdl/timer.v
+++ hdl/timer.v
@@ -5,7 +5,6 @@ module timer(clk, bC, bL, bD, bR, out_o, curr_digit, edit, done);
     output reg [2:0]curr_digit;
     output reg edit, done;
     reg edit_mode;
-    reg [35:0] timeReg;
 
     initial begin
         out_o <= 0;
@@ -16,12 +15,13 @@ module timer(clk, bC, bL, bD, bR, out_o, curr_digit, edit, done);
     always @(posedge clk_i) begin
         if(!edit_mode) begin
             if(bL) edit_mode <= 1;
-            else if(bD && !edit_mode) begin
+            else if(bD) begin
                 out_o <= 0;
                 edit_mode <= 1;
             end
             
             else if(out_o == 0) done <= 1;
+            
 
             else begin
                 // The maximum for each 4bit segment is 9 = 1001. If 1001, then increment the next segment
@@ -65,7 +65,8 @@ module timer(clk, bC, bL, bD, bR, out_o, curr_digit, edit, done);
             end //end if
             
         else if(edit_mode) begin //Edit mode ( ; _ ;) Only gonna give control down to the minute (down to the second for the timer!)
-			timeReg[11:0] <= 0;
+			clock_o[11:0] <= 0;
+			done <= 0;
 			
 			if(bL) begin 
 				if(currDigit == 0) begin currDigit <= 0; edit_mode <= 0; end
@@ -79,62 +80,62 @@ module timer(clk, bC, bL, bD, bR, out_o, curr_digit, edit, done);
 				case(currDigit)
 					3'b000:	begin   //HrR
 						if(bC) begin
-							if(timeReg[35:32] == 4'b1001) timeReg[35:32] <= 0;
-							else timeReg[35:32] <= timeReg[35:32] + 4'b0001;
+							if(out_o[35:32] == 4'b1001) out_o[35:32] <= 0;
+							else out_o[35:32] <= out_o[35:32] + 4'b0001;
 						end
 						else if(bD) begin
-							if(timeReg[35:32] == 4'b0000) timeReg[35:32] <= 4'b1001;
-							else timeReg[35:32] <= timeReg[35:32] - 4'b0001;
+							if(out_o[35:32] == 4'b0000) out_o[35:32] <= 4'b1001;
+							else out_o[35:32] <= out_o[35:32] - 4'b0001;
 						end
 					end
 					3'b001: begin   //HrR
 					   if(bC) begin
-					       if(timeReg[31:28] == 4'b1001) timeReg[31:28] <= 0;
-					       else timeReg[31:28] <= timeReg[31:28] + 4'b0001;
+					       if(out_o[31:28] == 4'b1001) out_o[31:28] <= 0;
+					       else out_o[31:28] <= out_o[31:28] + 4'b0001;
 					   end
 					   else if(bD) begin
-					       if(timeReg[31:28] == 4'b1001) timeReg[31:28] <= 0;
-							else timeReg[31:28] <= timeReg[31:28] + 4'b0001;
+					       if(out_o[31:28] == 4'b1001) out_o[31:28] <= 0;
+							else out_o[31:28] <= out_o[31:28] + 4'b0001;
 					   end
 					end
 					3'b010:	begin	//MinL
 						if(bC) begin
-							if(timeReg[27:24] == 4'b0101) timeReg[27:24] <= 0;
-							else timeReg[27:24] <= timeReg[27:24] + 4'b0001;
+							if(out_o[27:24] == 4'b0101) out_o[27:24] <= 0;
+							else out_o[27:24] <= out_o[27:24] + 4'b0001;
 						end
 						else if(bD) begin
-							if(timeReg[27:24] == 0) timeReg[27:24] <= 4'b0101;
-							else timeReg[27:24] <= timeReg[27:24] - 4'b0001;
+							if(out_o[27:24] == 0) out_o[27:24] <= 4'b0101;
+							else out_o[27:24] <= out_o[27:24] - 4'b0001;
 						end
 					end
 					3'b011:	begin
 						if(bC) begin
-							if(timeReg[23:20] == 4'b1001) timeReg[23:20] <= 0;
-							else timeReg[23:20] <= timeReg[23:20] + 4'b0001;
+							if(out_o[23:20] == 4'b1001) out_o[23:20] <= 0;
+							else out_o[23:20] <= out_o[23:20] + 4'b0001;
 			 			end
 	           			else if(bD) begin
-						    if(timeReg[23:20] == 0) timeReg[23:20] <= 4'b1001;
-						    else timeReg[23:20] <= timeReg[23:20] - 4'b0001;
+						    if(out_o[23:20] == 0) out_o[23:20] <= 4'b1001;
+						    else out_o[23:20] <= out_o[23:20] - 4'b0001;
 				    	end
 				    end
 				    3'b100: begin
 				        if(bC) begin
-							if(timeReg[19:16] == 4'b1001) timeReg[19:16] <= 0;
-							else timeReg[19:16] <= timeReg[19:16] + 4'b0001;
+							if(out_o[19:16] == 4'b1001) out_o[19:16] <= 0;
+							else out_o[19:16] <= out_o[19:16] + 4'b0001;
 			 			end
 	           			else if(bD) begin
-						    if(timeReg[19:16] == 0) timeReg[19:16] <= 4'b1001;
-						    else timeReg[19:16] <= timeReg[19:16] - 4'b0001;
+						    if(out_o[19:16] == 0) out_o[19:16] <= 4'b1001;
+						    else out_o[19:16] <= out_o[19:16] - 4'b0001;
 				    	end
 				    end
 				    3'b101: begin
 				        if(bC) begin
-							if(timeReg[15:12] == 4'b1001) timeReg[15:12] <= 0;
-							else timeReg[15:12] <= timeReg[15:12] + 4'b0001;
+							if(out_o[15:12] == 4'b1001) out_o[15:12] <= 0;
+							else out_o[15:12] <= out_o[15:12] + 4'b0001;
 			 			end
 	           			else if(bD) begin
-						    if(timeReg[15:12] == 0) timeReg[15:12] <= 4'b1001;
-						    else timeReg[15:12] <= timeReg[15:12] - 4'b0001;
+						    if(out_o[15:12] == 0) out_o[15:12] <= 4'b1001;
+						    else out_o[15:12] <= out_o[15:12] - 4'b0001;
 				    	end
 				    end
 				endcase
